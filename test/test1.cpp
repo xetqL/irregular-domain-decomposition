3e4db3effb493045c46695f265d64762c50f40c1
Anthony <anthony.boulmier@unige.ch>
2019-11-06 14:04:52 +0100

    tests and project organization

---
+++ test/test1.cpp
@@ -0,0 +1,28 @@
+//
+// Created by xetql on 11/6/19.
+//
+
+#include <gtest/gtest.h>
+
+namespace {
+
+#include <string>
+using std::string;
+
+const char *actualValTrue  = "hello gtest";
+const char *actualValFalse = "hello world";
+const char *expectVal      = "hello gtest";
+
+TEST(StrCompare, CStrEqual) {
+    EXPECT_STREQ(expectVal, actualValTrue);
+}
+
+TEST(StrCompare, CStrNotEqual) {
+    EXPECT_STREQ(expectVal, actualValTrue);
+}
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
